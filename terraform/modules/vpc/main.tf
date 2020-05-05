e819886a700157ea2d58843eda6cdb5cf72af722
eugenebbr <eugene.bbr@gmail.com>
2020-05-05 17:51:03 +0300

    Homework N12 main tasks completed (roles and envs)

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -8,6 +8,16 @@ resource "google_compute_firewall" "firewall_puma" {
   source_ranges = ["0.0.0.0/0"]
   target_tags   = ["reddit-app"]
 }
+resource "google_compute_firewall" "firewall_nginx" {
+  name    = "allow-puma-http-default"
+  network = "default"
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["reddit-app"]
+}
 resource "google_compute_firewall" "firewall_mongo" {
   name    = "allow-mongo-default"
   network = "default"
